89e86cc0ba1c56aec443806b82975b499ca548b0
Lingyu Li <lilingyu1991@foxmail.com>
2015-10-21 14:43:49 +0800

    Initial Commit

---
+++ permutation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  permutation
+//
+//  Created by LiLingyu on 15/10/21.
+//  Copyright © 2015年 LiLingyu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
